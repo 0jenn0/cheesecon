1e5cafbb770b0c325e741fdd7be7823906808eba
choi river <130737187+0jenn0@users.noreply.github.com>
2025-07-29 20:44:01 +0900

    feat: 필요없는 스타일들 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import ReactQueryClientProvider from '@/provider/QueryProvider';
 import '../shared/ui/styles/globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -26,11 +15,7 @@ export default function RootLayout({
   return (
     <ReactQueryClientProvider>
       <html lang='en'>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
+        <body className='antialiased'>{children}</body>
       </html>
     </ReactQueryClientProvider>
   );
