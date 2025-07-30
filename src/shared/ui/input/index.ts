6bea349682fee1c66e2fe9dc7dbcff499b3ae1ab
choi river <130737187+0jenn0@users.noreply.github.com>
2025-07-31 04:07:48 +0900

    feat: Textarea 컴포넌트 구현

--- src/shared/ui/input/index.ts
+++ src/shared/ui/input/index.ts
@@ -4,4 +4,5 @@ export { default as Label } from './label/label';
 export { default as ButtonStack } from './button-stack/button-stack';
 export { default as Checkbox } from './checkbox/checkbox';
 export { default as Placeholder } from './placeholder/placeholder';
+export { default as TextArea } from './text-area/text-area';
 export { default as TextField } from './text-field/text-field';
---
+++ src/shared/ui/input/text-area/text-area.tsx
@@ -0,0 +1,32 @@
+import { ComponentPropsWithRef } from 'react';
+import { cn } from '@/shared/lib/utils';
+import { textAreaVariants } from './tex-area.style';
+
+export interface TextAreaProps extends ComponentPropsWithRef<'textarea'> {
+  placeholder: string;
+  isError: boolean;
+  disabled: boolean;
+}
+
+export default function TextArea({
+  placeholder,
+  isError = false,
+  disabled = false,
+  className,
+  ...props
+}: TextAreaProps) {
+  return (
+    <textarea
+      {...props}
+      placeholder={placeholder}
+      className={cn(
+        'text-body-sm',
+        textAreaVariants({
+          variant: isError ? 'error' : 'default',
+        }),
+        className,
+      )}
+      disabled={disabled}
+    />
+  );
+}
