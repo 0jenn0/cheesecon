d7350e964d8c5c2084ff2f4640b44ed86f506feb
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-01 07:04:45 +0900

    test Modal 컴포넌트 storybook 작성

---
+++ src/shared/ui/feedback/modal/modal.stories.tsx
@@ -0,0 +1,57 @@
+import { Meta, StoryObj } from '@storybook/nextjs';
+import Button from '../../input/button/button';
+import Modal from './modal';
+import useModal, { ModalProvider } from './modal-provider';
+
+export default {
+  title: 'UI/Feedback/Modal',
+  component: Modal.Root,
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [
+    (Story) => (
+      <ModalProvider>
+        <Story />
+      </ModalProvider>
+    ),
+  ],
+} as Meta<typeof Modal.Root>;
+
+export const Default: StoryObj<typeof Modal.Root> = {
+  args: {
+    isOpen: true,
+  },
+  render: () => {
+    const { openModal, closeModal } = useModal();
+
+    return (
+      <>
+        <Button onClick={openModal}>모달 열기</Button>
+        <Modal.Root>
+          <Modal.Header>
+            <h2 className='text-lg font-semibold'>모달 제목</h2>
+          </Modal.Header>
+
+          <Modal.Body>
+            <p>모달 내용이 여기에 들어갑니다.</p>
+          </Modal.Body>
+
+          <Modal.Footer className='flex justify-between gap-8'>
+            <Button
+              variant='secondary'
+              styleVariant='transparent'
+              onClick={closeModal}
+              className='w-full'
+            >
+              취소
+            </Button>
+            <Button className='w-full' onClick={closeModal}>
+              확인
+            </Button>
+          </Modal.Footer>
+        </Modal.Root>
+      </>
+    );
+  },
+};
