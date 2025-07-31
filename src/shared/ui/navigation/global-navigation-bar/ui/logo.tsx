19f6d72dfcab29e4fd09d81bec58dc490b447c61
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-01 04:11:47 +0900

    feat: Gnb의 로고 누를시 홈으로 이동하기 추가

--- src/shared/ui/navigation/global-navigation-bar/ui/logo.tsx
+++ src/shared/ui/navigation/global-navigation-bar/ui/logo.tsx
@@ -1,14 +1,15 @@
+import Link from 'next/link';
 import { Icon } from '@/shared/ui/display';
 
 export default function Logo() {
   return (
-    <div className='flex items-center gap-8'>
+    <Link href='/' className='flex items-center gap-8'>
       <Icon
         name='logo'
         size={32}
         className='text-[var(--color-cheesecon-primary-300)]'
       />
       <span className='text-heading-sm'>CheeseCon</span>
-    </div>
+    </Link>
   );
 }
