6a32df45d67fb43c7a8920eb337150653e5a5370
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-01 09:30:39 +0900

    feat: Auth Provider 구현

---
+++ src/feature/auth/config/providers.ts
@@ -0,0 +1,22 @@
+import { ICON_NAMES } from '@/shared/ui/icon/config';
+
+export const LOGIN_PROVIDER = ['google', 'kakao', 'naver'] as const;
+export type LoginProvider = (typeof LOGIN_PROVIDER)[number];
+
+export const PROVIDER_CONFIG = {
+  google: {
+    text: '구글',
+    icon: 'google-login',
+  },
+  kakao: {
+    text: '카카오',
+    icon: 'kakao-login',
+  },
+  naver: {
+    text: '네이버',
+    icon: 'naver-login',
+  },
+} satisfies Record<
+  LoginProvider,
+  { text: string; icon: (typeof ICON_NAMES)[number] }
+>;
