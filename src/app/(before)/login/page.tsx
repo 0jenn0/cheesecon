3fe897b572a192ac46a27f2d44275e0315dc342d
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-01 09:32:13 +0900

    feat: 로그인 버튼에 로그인 함수 달기

--- src/app/(before)/login/page.tsx
+++ src/app/(before)/login/page.tsx
@@ -1,9 +1,12 @@
 'use client';
 
 import { Icon } from '@/shared/ui/display';
-import { LoginButton } from '@/feature/login/ui';
+import { useAuth } from '@/feature/auth/provider/auth-provider';
+import { LoginButton } from '@/feature/auth/ui';
 
 export default function LoginPage() {
+  const { signInWithProvider } = useAuth();
+
   return (
     <div className='padding-16 tablet:padding-24 flex h-full w-full flex-col items-center justify-center'>
       <div className='flex w-full flex-1 items-center justify-center'>
@@ -14,9 +17,15 @@ export default function LoginPage() {
       </div>
 
       <div className='flex w-full flex-col items-center gap-12'>
-        <LoginButton provider='naver' onClick={() => {}} />
-        <LoginButton provider='kakao' onClick={() => {}} />
-        <LoginButton provider='google' onClick={() => {}} />
+        {/* <LoginButton provider='naver' onClick={() => {}} /> */}
+        <LoginButton
+          provider='kakao'
+          onClick={() => signInWithProvider('kakao')}
+        />
+        <LoginButton
+          provider='google'
+          onClick={() => signInWithProvider('google')}
+        />
       </div>
     </div>
   );
