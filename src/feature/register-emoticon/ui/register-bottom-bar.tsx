2d8e5c1fa3d9e14c2cc2f7d8ae2aff26ffc122cd
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-02 07:51:05 +0900

    style: backdrop blur 정도 수정

--- src/feature/register-emoticon/ui/register-bottom-bar.tsx
+++ src/feature/register-emoticon/ui/register-bottom-bar.tsx
@@ -2,11 +2,8 @@ import { Button } from '@/shared/ui/input';
 
 export function RegisterBottomBar() {
   return (
-    <div className='fixed right-0 bottom-0 left-0 flex items-center justify-end bg-white/60 px-24 py-16 backdrop-blur-sm'>
-      <Button
-        textClassName='text-body-lg font-semibold'
-        className='padding-x-24'
-      >
+    <div className='fixed right-0 bottom-0 left-0 flex items-center justify-end bg-white/60 px-24 py-16 backdrop-blur-lg'>
+      <Button textClassName='text-body-lg font-semibold' className='padding-32'>
         등록하기
       </Button>
     </div>
