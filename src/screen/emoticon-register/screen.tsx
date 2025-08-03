178321f68212127f96b45c7121bfeb54395676cc
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-04 06:53:05 +0900

    fix: 누락된 provider 추가

--- src/screen/emoticon-register/screen.tsx
+++ src/screen/emoticon-register/screen.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { EmoticonRegisterProvider } from '@/feature/register-emoticon/model/hook';
 import { EmoticonProvider } from '@/feature/register-emoticon/ui/emoticon-section/provider';
 import EmoticonRegisterMobileScreen from './mobile-screen';
 import { EmoticonRegisterContextProvider } from './model/emoticon-register-context';
@@ -8,14 +9,16 @@ import { EmoticonRegisterDesktopScreen } from './responsive/desktop-screen';
 export default function EmoticonRegisterScreen() {
   return (
     <EmoticonRegisterContextProvider>
-      <EmoticonProvider>
-        <div className='tablet:hidden block'>
-          <EmoticonRegisterMobileScreen />
-        </div>
-        <div className='tablet:block hidden'>
-          <EmoticonRegisterDesktopScreen />
-        </div>
-      </EmoticonProvider>
+      <EmoticonRegisterProvider>
+        <EmoticonProvider>
+          <div className='tablet:hidden block'>
+            <EmoticonRegisterMobileScreen />
+          </div>
+          <div className='tablet:block hidden'>
+            <EmoticonRegisterDesktopScreen />
+          </div>
+        </EmoticonProvider>
+      </EmoticonRegisterProvider>
     </EmoticonRegisterContextProvider>
   );
 }
