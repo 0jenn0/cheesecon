d7670e5bffd6cc382f07f691dce14e07eba9be97
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-03 10:52:24 +0900

    fix: 이모티콘 imageUrl을 register provider 상태에 연동

--- src/feature/register-emoticon/ui/emoticon-section/ui/emoticon-grid.tsx
+++ src/feature/register-emoticon/ui/emoticon-section/ui/emoticon-grid.tsx
@@ -12,6 +12,7 @@ export default function EmoticonGrid() {
   const handleImageUpload = useCallback(
     (imageNumber: number, imageUrl: string) => {
       handleEmoticonItem(imageNumber, 'UPLOAD', { imageUrl });
+      handleSetImageUrl([{ imageUrl, imageOrder: imageNumber }]);
     },
     [handleEmoticonItem],
   );
