006b01a6933d771a8786f68652aceaf164190e99
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-04 20:36:30 +0900

    feat: 활동순 페이지 UI 구현 및 무한스크롤 추가

---
+++ src/feature/view-profile/index.ts
@@ -0,0 +1,4 @@
+export { default as ProfileViewItem } from './profile-view-item';
+export { default as ProfileViewSection } from './profile-view-section';
+export { default as ProfileViewSkeleton } from './profile-view-skeleton';
+export { default as ProfileViewFallback } from './profile-view-fallback';
--- src/screen/main/index.ts
+++ src/screen/main/index.ts
@@ -1,2 +1,3 @@
 export { default as PopularScreen } from './popular-screen';
 export { default as NewScreen } from './new-screen';
+export { default as ActivityScreen } from './activity-screen';
