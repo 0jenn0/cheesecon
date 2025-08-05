f31c08bc87c50dc1e2729fa1b9dc02cf53155a95
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-06 06:39:16 +0900

    style: 인사 컴포넌트에 아이콘 추가

--- src/screen/main/ui/hello-section.tsx
+++ src/screen/main/ui/hello-section.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { Icon } from '@/shared/ui/display';
 import { Button } from '@/shared/ui/input';
 import { useAuth } from '@/feature/auth/provider/auth-provider';
 
@@ -10,12 +11,17 @@ export default function HelloSection() {
 
   return (
     <section className='padding-16 bg-primary tablet:border-radius-xl tablet:flex-row tablet:justify-between flex w-full flex-col gap-16'>
-      <div className='flex flex-col gap-4'>
-        <h1 className='text-body-lg font-semibold'>
-          {user?.name} 작가님, 안녕하세요!
-        </h1>
-        <p className='text-body-sm text-secondary'>오늘도 응원해요!</p>
+      <div className='flex items-center gap-16'>
+        <Icon name='logo' className='icon-ghost h-[60px] w-[60px]' />
+
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-body-lg font-semibold'>
+            {user?.name} 작가님, 안녕하세요!
+          </h1>
+          <p className='text-body-sm text-secondary'>오늘도 응원해요!</p>
+        </div>
       </div>
+
       <Button
         variant='primary'
         leadingIcon='edit'
