d69c6880a0feea4e13a45dc8e3e7536ee36dfc37
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-07 23:04:08 +0900

    fix: kakao->kakaotalk 수정 반영

--- src/screen/emoticon/ui/emoticon-info-section/ui/emoticon-platform-item.tsx
+++ src/screen/emoticon/ui/emoticon-info-section/ui/emoticon-platform-item.tsx
@@ -1,27 +1,30 @@
-import { EmoticonSet } from "@/entity/emoticon-set/type";
-import { cn } from "@/shared/lib";
-import { Icon } from "@/shared/ui/display";
+import { cn } from '@/shared/lib';
+import { Icon } from '@/shared/ui/display';
+import { EmoticonSet } from '@/entity/emoticon-set/type';
 
-export default function EmoticonPlatformItem({ platform }: { platform: EmoticonSet['platform'] }) {
-    return (
-      <div
+export default function EmoticonPlatformItem({
+  platform,
+}: {
+  platform: EmoticonSet['platform'];
+}) {
+  return (
+    <div
+      className={cn(
+        'padding-8 border-radius-md tablet:w-[160px] flex w-full items-center gap-4',
+        platform === 'kakaotalk' ? 'bg-yellow-50' : 'bg-green-50',
+      )}
+    >
+      <Icon
+        name={platform === 'kakaotalk' ? 'kakao-logo' : 'line-logo'}
+        size={20}
+      />
+      <p
         className={cn(
-          'padding-8 border-radius-md tablet:w-[160px] flex w-full items-center gap-4',
-          platform === 'kakao' ? 'bg-yellow-50' : 'bg-green-50',
+          platform === 'kakaotalk' ? 'text-yellow-700' : 'text-green-700',
         )}
       >
-        <Icon
-          name={platform === 'kakao' ? 'kakao-logo' : 'line-logo'}
-          size={20}
-        />
-        <p
-          className={cn(
-            platform === 'kakao' ? 'text-yellow-00' : 'text-green-700',
-          )}
-        >
-          {platform === 'kakao' ? '카카오톡' : '라인'}
-        </p>
-      </div>
-    );
-  }
-  
+        {platform === 'kakaotalk' ? '카카오톡' : '라인'}
+      </p>
+    </div>
+  );
+}
