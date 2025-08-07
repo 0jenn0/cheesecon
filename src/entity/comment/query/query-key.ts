9ad942912d23519cc40d968930b753d9604f46f8
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-07 23:00:51 +0900

    refactor: 코멘트 쿼리 키 가독성 좋게 수정

--- src/entity/comment/query/query-key.ts
+++ src/entity/comment/query/query-key.ts
@@ -1,8 +1,8 @@
 export const COMMENT_QUERY_KEY = {
   all: ['comment'] as const,
   lists: () => [...COMMENT_QUERY_KEY.all, 'list'] as const,
-  list: (filters: string) =>
-    [...COMMENT_QUERY_KEY.lists(), { filters }] as const,
+  list: (filters: Record<string, string | number | null>) =>
+    [...COMMENT_QUERY_KEY.lists(), filters] as const,
   details: () => [...COMMENT_QUERY_KEY.all, 'detail'] as const,
   detail: (id: string) => [...COMMENT_QUERY_KEY.details(), id] as const,
 } as const;
