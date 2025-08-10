50e5241f9bff9ab38dcdf40e75c62b610061a332
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-10 10:52:13 +0900

    feat: 좋아요 이모티콘 세트 가져오는 api 추가

--- src/entity/emoticon-set/query/query-key.ts
+++ src/entity/emoticon-set/query/query-key.ts
@@ -1,16 +1,38 @@
-import { BaseApiRequest } from '@/shared/types';
+import { BaseApiRequest, SortOrder } from '@/shared/types';
+import { EmoticonSetOrderBy } from '../type';
 import { EmoticonSetQueryKey } from './types';
 
 type PaginationParams = Pick<BaseApiRequest, 'limit' | 'offset'>;
 
+type PageParams = {
+  page: number;
+  limit: number;
+  order: SortOrder;
+  userId?: string;
+  title?: string;
+  isLiked?: boolean;
+};
+
 export const EMOTICON_SET_QUERY_KEY: EmoticonSetQueryKey = {
   all: ['emoticon-sets'] as const,
-  order: (order: string, param?: PaginationParams) =>
+  order: (order: EmoticonSetOrderBy, param?: PaginationParams) =>
     [
       ...EMOTICON_SET_QUERY_KEY.all,
       order,
       param?.offset,
       param?.limit,
+    ] as const,
+  pagination: (order: EmoticonSetOrderBy, param: PageParams) =>
+    [
+      ...EMOTICON_SET_QUERY_KEY.all,
+      'pagination',
+      order,
+      param.page,
+      param.limit,
+      param.order,
+      param.userId,
+      param.title,
+      param.isLiked,
     ] as const,
   byId: (id: string) => [...EMOTICON_SET_QUERY_KEY.all, id] as const,
 };
