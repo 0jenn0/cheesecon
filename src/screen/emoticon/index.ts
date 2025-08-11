0eb2e597dd6cde75d69f892229afa44c4eb7fb1d
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-11 10:52:07 +0900

    feat: 이모티콘 게시물의 비밀글 여부 체크 서버 컴포넌트 추가

--- src/screen/emoticon/index.ts
+++ src/screen/emoticon/index.ts
@@ -1,1 +1,2 @@
 export { default as EmoticonScreen } from './emoticon-screen';
+export { default as PostEmoticonScreen } from './post-screen';
---
+++ src/screen/emoticon/post-screen.tsx
@@ -0,0 +1,13 @@
+import { Suspense } from 'react';
+import { getEmoticonSetDetail } from '@/entity/emoticon-set';
+import { EmoticonScreen } from '@/screen';
+
+export default async function PostEmoticonScreen({ setId }: { setId: string }) {
+  const emoticonData = await getEmoticonSetDetail(setId);
+
+  if (!emoticonData) return <div>이모티콘 세트를 찾을 수 없습니다.</div>;
+
+  const isPrivate = emoticonData.is_private ?? false;
+
+  return <EmoticonScreen emoticonSetId={setId} isUnlocked={true} />;
+}
