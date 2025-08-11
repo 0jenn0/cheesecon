155b15962cfcff3f342f5de742a897b6f358fdb0
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-12 01:19:26 +0900

    feat: imageId를 searchparam으로 수정 및 모든 이미지를 한번에 모달에 전달로 수정

--- src/app/(after)/emoticon/[id]/page.tsx
+++ src/app/(after)/emoticon/[id]/page.tsx
@@ -1,11 +1,32 @@
+import { getEmoticonSetDetail } from '@/entity/emoticon-set';
+import ViewEmoticonImageModal from '@/feature/view-emoticon-image/view-emoticon-image-modal';
 import { EmoticonScreen } from '@/screen';
 
-export default async function EmoticonPage({
+export default async function ImageFullPage({
   params,
+  searchParams,
 }: {
   params: Promise<{ id: string }>;
+  searchParams: Promise<{ imageId: string }>;
 }) {
   const { id } = await params;
+  const { imageId } = await searchParams;
 
-  return <EmoticonScreen emoticonSetId={id} isUnlocked={true} />;
+  const emoticonSetDetail = await getEmoticonSetDetail(id);
+  const authorId = emoticonSetDetail.user_id;
+  const allImages = emoticonSetDetail.emoticon_images;
+
+  return (
+    <>
+      {imageId ? (
+        <ViewEmoticonImageModal
+          allImages={allImages}
+          imageId={imageId}
+          authorId={authorId}
+        />
+      ) : (
+        <EmoticonScreen emoticonSetId={id} isUnlocked={true} />
+      )}
+    </>
+  );
 }
