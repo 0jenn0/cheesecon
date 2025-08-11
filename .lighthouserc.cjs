03afbf9eab022409779321ce43788bbf4affe27d
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-11 22:56:20 +0900

    feat: 개선 전 개별 이미지 모달 성능 측정

---
+++ .lighthouserc.cjs
@@ -0,0 +1,25 @@
+// .lighthouserc.js
+module.exports = {
+  ci: {
+    collect: {
+      url: [
+        'http://localhost:3000/emoticon/255d2134-830c-48ed-aa05-cc6e019445aa/fe19f6af-6851-4eb1-956b-ddee439871c1',
+      ],
+      numberOfRuns: 1,
+      settings: {
+        chromeFlags: [
+          '--no-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-web-security',
+          '--disable-features=TranslateUI',
+          '--disable-ipc-flooding-protection',
+          '--disable-background-timer-throttling',
+          '--disable-backgrounding-occluded-windows',
+          '--disable-renderer-backgrounding',
+          '--ignore-certificate-errors',
+          '--allow-running-insecure-content',
+        ],
+      },
+    },
+  },
+};
