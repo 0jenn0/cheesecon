284496f23472a5c6d8847e39381e6ef73a981eac
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-12 23:09:32 +0900

    chore: 안쓰는 파일 삭제

--- src/feature/comment/ui/comment-react-form.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentPropsWithRef } from 'react';
-import { cn } from '@/shared/lib/utils';
-
-export default function CommentReactForm({
-  className,
-  ...props
-}: ComponentPropsWithRef<'div'>) {
-  return <div className={cn(className)} {...props}></div>;
-}
