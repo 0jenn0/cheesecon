cc4c600732511e9fa2ae0608eca6318c4d3834de
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-13 13:37:54 +0900

    hotfix: 모달이 값이 준비됏을때 열리도록 수정

--- src/app/(after)/emoticon/[id]/page.tsx
+++ src/app/(after)/emoticon/[id]/page.tsx
@@ -24,7 +24,7 @@ export default async function ImageFullPage({
 
   return (
     <>
-      {imageId ? (
+      {imageId && authorId && imagesRes?.success ? (
         <ViewEmoticonImageModal
           allImages={allImages ?? []}
           authorId={authorId}
