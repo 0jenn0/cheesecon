a429db644fe88cf3b0a1d26545c9d1a4eccce2b7
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-14 18:26:10 +0900

    feat: 메인 페이지에 사용되넌 뷰 컴포넌트들 client,server용으로 따로 분리

---
+++ src/feature/view-emotion/emoticon-view-section/emoticon-view-section.server.tsx
@@ -0,0 +1,22 @@
+import { EmoticonSetWithRepresentativeImage } from '@/entity/emoticon-set';
+import { EmoticonViewItemServer } from '@/feature/view-emotion/emoticon-view-section/ui';
+
+export default function EmoticonViewSectionServer({
+  initial,
+}: {
+  initial?: EmoticonSetWithRepresentativeImage[];
+}) {
+  return (
+    <ul className='grid grid-cols-1 gap-x-24 gap-y-12 md:grid-cols-2'>
+      {initial?.map((item, index) => (
+        <li
+          className='flex cursor-pointer flex-col gap-12'
+          key={`${item.id}-${index}`}
+        >
+          <EmoticonViewItemServer item={item} index={index + 1} />
+          <div className='border-ghost w-full border-b-[0.6px]' />
+        </li>
+      ))}
+    </ul>
+  );
+}
--- src/feature/view-emotion/emoticon-view-section/ui/index.ts
+++ src/feature/view-emotion/emoticon-view-section/ui/index.ts
@@ -1,1 +1,3 @@
+export { default as EmoticonViewItemClient } from './emoticon-view-item.client';
+export { default as EmoticonViewItemServer } from './emoticon-view-item.server';
 export { default as EmoticonViewItemSkeleton } from './emoticon-view-item.skeleton';
