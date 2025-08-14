305545f48822d7bc947ef5afee49eb7b7c7b43e1
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-14 18:53:23 +0900

    chore: 안쓰는 파일 삭제

--- src/feature/view-profile/index.ts
+++ src/feature/view-profile/index.ts
@@ -1,4 +1,3 @@
 export { default as ProfileViewItem } from './profile-view-item';
-export { default as ProfileViewSection } from './profile-view-section';
 export { default as ProfileViewSkeleton } from './profile-view-skeleton';
 export { default as ProfileViewFallback } from './profile-view-fallback';
--- src/screen/index.ts
+++ src/screen/index.ts
@@ -1,4 +1,3 @@
 export { default as EmoticonRegisterScreen } from './emoticon-register/screen';
 export { default as EmoticonRegisterMobileScreen } from './emoticon-register/mobile-screen';
-export * from './main';
 export * from './emoticon';
