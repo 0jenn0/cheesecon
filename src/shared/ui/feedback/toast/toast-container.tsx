c5ba71e5218b2aba896ea7b04e2f82ae779a1249
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-16 15:08:06 +0900

    feat: 토스트 위치 추가

--- src/shared/ui/feedback/toast/toast-container.tsx
+++ src/shared/ui/feedback/toast/toast-container.tsx
@@ -1,17 +1,45 @@
 'use client';
 
+import { cva } from 'class-variance-authority';
 import Toast from './toast';
 import { useToast } from './toast-provider';
+
+const toastContainerVariants = cva(
+  'z-index-popover fixed right-24 flex flex-col gap-8',
+  {
+    variants: {
+      position: {
+        top: 'top-24',
+        bottom: 'bottom-24',
+      },
+    },
+  },
+);
 
 export default function ToastContainer() {
   const { toasts } = useToast();
   if (toasts.length === 0) return null;
 
+  const topToasts = toasts.filter((toast) => toast.position === 'top');
+  const bottomToasts = toasts.filter((toast) => toast.position !== 'top');
+
   return (
-    <div className='z-index-popover fixed right-24 bottom-24 flex flex-col gap-8'>
-      {toasts.map((toast) => (
-        <Toast key={toast.id} {...toast} />
-      ))}
-    </div>
+    <>
+      {topToasts.length > 0 && (
+        <div className={toastContainerVariants({ position: 'top' })}>
+          {topToasts.map((toast) => (
+            <Toast key={toast.id} {...toast} />
+          ))}
+        </div>
+      )}
+
+      {bottomToasts.length > 0 && (
+        <div className={toastContainerVariants({ position: 'bottom' })}>
+          {bottomToasts.map((toast) => (
+            <Toast key={toast.id} {...toast} />
+          ))}
+        </div>
+      )}
+    </>
   );
 }
