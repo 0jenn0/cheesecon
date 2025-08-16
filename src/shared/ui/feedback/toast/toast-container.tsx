637e25ab68842ca908ee57f062a7605f2f220f65
choi river <130737187+0jenn0@users.noreply.github.com>
2025-08-16 11:51:01 +0900

    style: 토스트 애니메이션 수정

--- src/shared/ui/feedback/toast/toast-container.tsx
+++ src/shared/ui/feedback/toast/toast-container.tsx
@@ -1,15 +1,15 @@
 'use client';
 
+import { AnimatePresence, motion } from 'framer-motion';
 import Toast from './toast';
 import { useToast } from './toast-provider';
 
 export default function ToastContainer() {
   const { toasts } = useToast();
-
   if (toasts.length === 0) return null;
 
   return (
-    <div className='z-index-popover fixed right-24 bottom-24 flex flex-col'>
+    <div className='z-index-popover fixed right-24 bottom-24 flex flex-col gap-8'>
       {toasts.map((toast) => (
         <Toast key={toast.id} {...toast} />
       ))}
