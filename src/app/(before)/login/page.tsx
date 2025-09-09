bf31544fc3853bfad7245d26bd669f4df2046f05
choi river <130737187+0jenn0@users.noreply.github.com>
2025-09-09 13:26:45 +0900

    fix: 로그인 화면 metadata description 수정

--- src/app/(before)/login/page.tsx
+++ src/app/(before)/login/page.tsx
@@ -5,7 +5,7 @@ import LoginScreen from '@/screen/login/login-screen';
 export const metadata: Metadata = {
   title: '로그인',
   description:
-    '커뮤니티에서 가장 많은 사랑을 받고 있는 이모티콘들을 만나보세요. 좋아요와 피드백이 가장 많은 인기작들을 모았습니다.',
+    '이모티콘 피드백 커뮤니티 치즈콘에 로그인하여 피드백을 받아보세요!',
   keywords: [
     '인기 이모티콘',
     '좋아요 많은 이모티콘',
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: '로그인',
     description:
-      '커뮤니티에서 가장 많은 사랑을 받고 있는 이모티콘들을 만나보세요. 좋아요와 피드백이 가장 많은 인기작들을 모았습니다.',
+      '이모티콘 피드백 커뮤니티 치즈콘에 로그인하여 피드백을 받아보세요!',
     url: 'https://cheesecon.kr/login',
   },
   alternates: {
