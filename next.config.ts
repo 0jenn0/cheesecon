533e79dcce1a58d0db5b567e7e79f2b260d9b422
choi river <130737187+0jenn0@users.noreply.github.com>
2025-09-10 11:20:59 +0900

    fix: 배포 시 ffmpeg/ffprobe  바이너리를 번들에 포함하도록 수정

--- next.config.ts
+++ next.config.ts
@@ -66,7 +66,7 @@ const nextConfig: NextConfig = {
     ],
   },
   outputFileTracingIncludes: {
-    'src/server/media/ffmpeg.ts': [ffm.path, ffp.path],
+    'src/shared/lib/media/ffmpeg.ts': [ffm.path, ffp.path],
   },
   webpack: (config) => {
     // @ts-expect-error
