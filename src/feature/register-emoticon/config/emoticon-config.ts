d327123b701f951bcd8ce81c2ec694b82a8fa2d0
choi river <130737187+0jenn0@users.noreply.github.com>
2025-09-10 10:17:48 +0900

    feat: 이모티콘 등록 config 원래대로 수정

--- src/feature/register-emoticon/config/emoticon-config.ts
+++ src/feature/register-emoticon/config/emoticon-config.ts
@@ -7,27 +7,21 @@ export type EmoticonType = (typeof EMOTICON_TYPE)[number];
 export const EMOTICON_CONFIG = {
   kakaotalk: {
     static: {
-      // FIXME :개발시에만 2개
-      count: 2,
+      count: 32,
       size: 320,
     },
     animated: {
-      // FIXME :개발시에만 2개
-      count: 2,
-      // count: 24,
+      count: 24,
       size: 320,
     },
   },
   ogq: {
     static: {
-      // FIXME :개발시에만 2개
-      // count: 32,
-      count: 2,
+      count: 32,
       size: 320,
     },
     animated: {
-      // count: 24,
-      count: 2,
+      count: 24,
       size: 320,
     },
   },
